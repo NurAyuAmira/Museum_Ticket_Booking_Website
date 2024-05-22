318be4e4918b04cc1e607b0223b9ce492e9a5175
Nur Ayu Amira Binti Idris <yuz9690@gmail.com>
2024-05-22 22:49:13 +0800

    Update backup_20240522_153638.sql

--- backup_20240522_153638.sql
+++ backup_20240522_153638.sql
@@ -1,4 +1,5 @@
 
+USE museum;
 
 CREATE TABLE `activity_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -7,7 +8,7 @@ CREATE TABLE `activity_logs` (
   `activity_details` text DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO activity_logs VALUES("1", "1", "Login attempt", "User Logged In", "2024-05-22 12:26:40");
 INSERT INTO activity_logs VALUES("2", "1", "Logout", "User logged out", "2024-05-22 12:27:41");
@@ -22,8 +23,6 @@ INSERT INTO activity_logs VALUES("10", "2", "Failed login attempt", "Incorrect p
 INSERT INTO activity_logs VALUES("11", "2", "Failed login attempt", "Incorrect password", "2024-05-22 14:37:56");
 INSERT INTO activity_logs VALUES("12", "2", "Login attempt", "User Logged In", "2024-05-22 14:38:13");
 INSERT INTO activity_logs VALUES("13", "2", "Failed login attempt", "Incorrect password", "2024-05-22 14:38:13");
-INSERT INTO activity_logs VALUES("14", "3", "Login attempt", "User Logged In", "2024-05-22 21:26:32");
-INSERT INTO activity_logs VALUES("15", "3", "Failed login attempt", "Incorrect password", "2024-05-22 21:26:32");
 
 
 
@@ -38,10 +37,9 @@ CREATE TABLE `admins` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO admins VALUES("2", "admin_test", "$2y$10$1dwbvmODobu0syMklQmTFO0IRUZHoj/eI9KpX4Ko/l7iLmUmjF8Q.", "admin@gmail.com", "2024-05-22 13:47:15");
-INSERT INTO admins VALUES("3", "mannoj", "$2y$10$jwtMehBJOULIVTKA6X5l9.PUeoW45rUd3SJP//1bA6Xcj.Mp.Wz7G", "hunkmoron7@gmail.com", "2024-05-22 21:28:19");
+INSERT INTO admins VALUES("2", "admin", "$2y$10$1dwbvmODobu0syMklQmTFO0IRUZHoj/eI9KpX4Ko/l7iLmUmjF8Q.", "hunkmoron7@gmail.com", "2024-05-22 13:47:15");
 
 
 
@@ -55,12 +53,39 @@ CREATE TABLE `exhibits` (
   `tickets_available` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO exhibits VALUES("2", "Telecommunication Exhibits", "At Museum Telekom", "2024-05-24", "494", "3.00");
+INSERT INTO exhibits VALUES("2", "Telecommunication Exhibits", "At Museum Telekom", "2024-05-24", "497", "3.00");
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO users VALUES("2", "ayu", "$2y$10$MXwF2Ha720pOZMTA57Vtg.RcVdFChrlFA3jfR4aauGNfJ0Oih/0LW", "yuzoktober@gmail.com", "2024-05-22 14:26:00");
 
 
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `exhibit_id` int(11) DEFAULT NULL,
+  `num_tickets` int(11) NOT NULL,
+  `reservation_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `invoice_path` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `exhibit_id` (`exhibit_id`),
+  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`exhibit_id`) REFERENCES `exhibits` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO reservations VALUES("1", "2", "2", "3", "2024-05-22 14:45:38", "NULL");
 
 
 CREATE TABLE `payments` (
@@ -74,33 +99,9 @@ CREATE TABLE `payments` (
   PRIMARY KEY (`id`),
   KEY `reservation_id` (`reservation_id`),
   CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO payments VALUES("1", "1", "cTlrb2x2L25NVFczZ29YTWZhZExvUDRlQW51bUJ3UklMREV4THUyYU1XMD06OmbMuUBAmnz6lMCRdm82O4E=", "XXXXXXXXXXXX4567", "12/XX", "XXX", "2024-05-22 14:48:24");
-INSERT INTO payments VALUES("2", "2", "d1BpbDhoNnNiLzY4a0d0TWtEQTFJUT09Ojr2OnbWoDCi1HZkhFXM90PX", "XXXXXXXXXXXX4567", "12/XX", "XXX", "2024-05-22 21:27:39");
-
-
-
-
-
-CREATE TABLE `reservations` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `exhibit_id` int(11) DEFAULT NULL,
-  `num_tickets` int(11) NOT NULL,
-  `reservation_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `exhibit_id` (`exhibit_id`),
-  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`exhibit_id`) REFERENCES `exhibits` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO reservations VALUES("1", "2", "2", "3", "2024-05-22 14:45:38");
-INSERT INTO reservations VALUES("2", "3", "2", "3", "2024-05-22 21:27:10");
-
-
-
 
 
 CREATE TABLE `traffic_monitor` (
@@ -110,32 +111,19 @@ CREATE TABLE `traffic_monitor` (
   `request_count` int(11) DEFAULT 1,
   `last_request` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO traffic_monitor VALUES("1", "NULL", "::1", "3", "2024-05-22 21:26:32");
-INSERT INTO traffic_monitor VALUES("2", "1", "::1", "3", "2024-05-22 21:26:32");
-INSERT INTO traffic_monitor VALUES("3", "2", "::1", "3", "2024-05-22 21:26:32");
-INSERT INTO traffic_monitor VALUES("4", "2", "::1", "3", "2024-05-22 21:26:32");
-INSERT INTO traffic_monitor VALUES("5", "2", "::1", "3", "2024-05-22 21:26:32");
-INSERT INTO traffic_monitor VALUES("6", "3", "::1", "1", "2024-05-22 21:26:32");
-
+INSERT INTO traffic_monitor VALUES("1", "NULL", "::1", "10", "2024-05-22 14:38:13");
+INSERT INTO traffic_monitor VALUES("2", "1", "::1", "10", "2024-05-22 14:38:13");
+INSERT INTO traffic_monitor VALUES("3", "2", "::1", "7", "2024-05-22 14:38:13");
+INSERT INTO traffic_monitor VALUES("4", "2", "::1", "5", "2024-05-22 14:38:13");
+INSERT INTO traffic_monitor VALUES("5", "2", "::1", "1", "2024-05-22 14:38:13");
 
 
 
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users VALUES("2", "user_tst", "$2y$10$MXwF2Ha720pOZMTA57Vtg.RcVdFChrlFA3jfR4aauGNfJ0Oih/0LW", "user@gmail.com", "2024-05-22 14:26:00");
-INSERT INTO users VALUES("3", "ayu", "$2y$10$jkBH7psH7K/.Kxq.Eusbt.7BTIn2KXZ3gVc/RhWzJlD99NzFUN8aq", "yuz9690@gmail.com", "2024-05-22 21:26:19");
+
 
 
 
